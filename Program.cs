5fda608eda4c05ce39e881fb61dd92eaea575e2b
Lucas Vazquez <vazquezlucasmatias2001@gmail.com>
2023-02-26 17:19:56 -0300

    Bucles while y do-while
    Finalizados

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@
  * codigo a repetir
  * }
  */
+
+/*Bucle Do-While. sintaxis
+*do{
+*codigo
+*}while(Condicion)
+*/
+
 using System;
 
 namespace PracticasBucles
@@ -12,7 +19,7 @@ namespace PracticasBucles
     {
         static void Main(string[] args)
         {
-
+            /*
             //Bucle WHILE
 
             Console.WriteLine("¿Deseas entrar en el bucle while?");
@@ -42,9 +49,72 @@ namespace PracticasBucles
 
             Random numero=new Random();
 
-            int numeroAleatorio=numero.Next(0,100);
+            int aleatorio=numero.Next(0,100);
 
-            Console.WriteLine(numeroAleatorio);
+            int minumero = 101;
+
+            int intentos = 0;
+
+            Console.WriteLine("Introduce un numero entre el 0 y 100");
+
+            while(aleatorio!= minumero)
+            {
+
+                intentos++;
+
+                minumero=int.Parse(Console.ReadLine());
+
+                if (minumero > aleatorio) Console.WriteLine("El numero es mas bajo");
+
+                if (minumero < aleatorio) Console.WriteLine("El numero es mas alto");
+
+            }
+
+            Console.WriteLine($"Correcto! Has necesitado {intentos} intentos");
+
+            //Bucle do-while
+
+            int z = 10;
+
+
+            do
+            {
+                Console.WriteLine("Impresión " + z);
+
+                z++;
+            } while (z < 15);
+
+            */
+
+            //Ejercicio de while pero con do-while
+
+            Random numero = new Random();
+
+            int aleatorio = numero.Next(0, 100);
+
+            int minumero;
+
+            int intentos = 0;
+
+            Console.WriteLine("Introduce un numero entre el 0 y 100");
+
+
+            do{
+                intentos++;
+
+                minumero = int.Parse(Console.ReadLine());
+
+                if (minumero > aleatorio) Console.WriteLine("El numero es mas bajo");
+
+                if (minumero < aleatorio) Console.WriteLine("El numero es mas alto");
+
+            } while (aleatorio != minumero);
+
+            Console.WriteLine($"Correcto! Has necesitado {intentos} intentos");
+
+            //
+
+
 
         }
     }
